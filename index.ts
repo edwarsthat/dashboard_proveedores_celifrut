ab2f054d6f99ba8a02d4699e28ee736895df0c5b
CelifrutD <transformaciondigital@celifrut.com>
2025-08-21 08:26:18 -0500

    primer commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import app from "./app/app.ts";
+import http from 'http';
+
+const server = http.createServer(app);
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+console.log(`Servidor configurado para escuchar en el puerto ${PORT} y la dirección IP ${HOST}`);
+
+(async () => {
+    try {
+
+        server.listen(PORT, HOST, () => {
+            console.log(`El servidor está escuchando en el puerto ${PORT} y la dirección IP ${HOST}.`);
+        });
+
+    } catch (err) {
+        console.error('Error al iniciar el servidor:', err);
+        process.exit(1);
+    }
+})();
+
+
+
