86c71867b456d7a34bc519e5d97dac90298096a0
Edwar Stheven <edwarsthat@gmail.com>
2025-08-21 10:56:40 -0500

    creo que estaba iniciadno dos veces el servidor

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -9,8 +10,5 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-});
 
 export default app;
